bf0c215729f08a06d033dc4d2e1ca7db4a5e0ca2
deguchi37458 <deguchi37458@gmail.com>
2023-05-12 17:58:54 +0900

    [add] sec1 animation

--- assets/scripts/pages/index.js
+++ assets/scripts/pages/index.js
@@ -3,26 +3,43 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 gsap.registerPlugin(ScrollTrigger)
 
-let secWrapperArray = document.querySelectorAll('.sec-wrapper')
+// let sec1 = document.querySelector('#sec-1')
+// let sec2 = document.querySelector('#sec-2')
+// let sec3 = document.querySelector('#sec-3')
 
+// sec-1
+let secTitle1Main = document.querySelector('.sec-1 .sec-title .main')
+let secTitle1bar = document.querySelector('.sec-1 .sec-title .bar')
 window.addEventListener('load', () => {
-  secWrapperArray.forEach(secWrapper => {
-    gsap.from(secWrapper, {
-      opacity: 0,
-      duration: 0.8
-    })
-  })
+  gsap
+    .timeline({})
+    .to(secTitle1bar, { scaleX: 1, duration: 0.5 })
+    .to(secTitle1bar, { transformOrigin: 'right', scaleX: 0, duration: 0.5 })
+    .to(secTitle1Main, { opacity: 1, duration: 0 }, '-=0.5')
 })
 
-secWrapperArray.forEach(secWrapper => {
-  gsap.from(secWrapper, {
-    scale: 0.8,
-    scrollTrigger: {
-      trigger: secWrapper,
-      start: 'top bottom',
-      end: 'top 60%',
-      scrub: true,
-      markers: true
-    }
-  })
+// sec-2
+let secWrapper2 = document.querySelector('.sec-2 .sec-wrapper')
+gsap.from(secWrapper2, {
+  scale: 2,
+  scrollTrigger: {
+    trigger: secWrapper2,
+    start: 'top bottom',
+    end: 'top 20%',
+    scrub: true,
+    markers: true
+  }
+})
+
+// sec-3
+let secTitle3 = document.querySelector('.sec-3 .sec-title')
+gsap.to(secTitle3, {
+  x: 1200,
+  scrollTrigger: {
+    trigger: secTitle3,
+    start: 'top bottom',
+    end: 'bottom top',
+    scrub: true,
+    markers: true
+  }
 })
